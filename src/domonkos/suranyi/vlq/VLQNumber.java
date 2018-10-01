b73fd3626927e650750876cccb91fc65a0c36433
Domonkos Suranyi <domonkos.suranyi@thyssenkrupp.com>
2018-10-01 18:03:08 +0200

    Implement fully fuctional VLQ API

--- src/domonkos/suranyi/vlq/VLQNumber.java
+++ src/domonkos/suranyi/vlq/VLQNumber.java
@@ -11,11 +11,11 @@ public class VLQNumber {
     }
 
     public VLQNumber(final int[] bytes) {
-        value = validateVLQ(bytes);
+        value = removeLeadingZeros(validateVLQ(bytes));
     }
 
     public VLQNumber(final byte[] bytes) {
-        value = checkLength(new int[bytes.length]);
+        value = removeLeadingZeros(checkLength(new int[bytes.length]));
         for (int i = 0; i < bytes.length; i++)
             value[i] = bytes[i] & 0xFF;
     }
@@ -43,15 +43,26 @@ public class VLQNumber {
     }
 
     private static final int bytes2Int(final int[] array) {
-        return Arrays.stream(array).reduce(0, (left, right) -> (left << 8) + right);
+        return Arrays.stream(array).reduce(0, (left, right) -> (left << 7) + (right - 0b1000_0000)) + 0b1000_0000;
     }
 
     private static final int[] int2Bytes(final int number) {
         int[] ret = new int[4];
-        ret[0] = (byte) ((number & 0xff000000) >> 24);
-        ret[1] = (byte) ((number & 0x00ff0000) >> 16);
-        ret[2] = (byte) ((number & 0x0000ff00) >> 8);
-        ret[3] = (byte) (number & 0x000000ff);
+        ret[0] = ((number & 0b0000_1111_1110_0000_0000_0000_0000_0000) >> 21);
+        ret[1] = ((number & 0b0001_1111_1100_0000_0000_0000) >> 14);
+        ret[2] = ((number & 0b0011_1111_1000_0000) >> 7);
+        ret[3] = (number & 0b0111_1111);
+
+        ret = removeLeadingZeros(ret);
+        for (int i = 0; i < ret.length - 1; i++)
+            ret[i] += 0b1000_0000;
         return ret;
+    }
+
+    private static final int[] removeLeadingZeros(final int[] array) {
+        for (int i = 0; i < 4; i++)
+            if (array[i] != 0)
+                return Arrays.copyOfRange(array, i, array.length);
+        return new int[] {0};
     }
 }
--- test/domonkos/suranyi/vlq/Test_VLQNumber.java
+++ test/domonkos/suranyi/vlq/Test_VLQNumber.java
@@ -4,18 +4,34 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class Test_VLQNumber {
 
     @Test
     public void test_asInt() {
-        assertEquals(0x00,  new VLQNumber(new byte[] {0x00, 0x00, 0x00, 0x00}).intoInt());
-        assertEquals(0x7F,  new VLQNumber(new byte[] {0x00, 0x00, 0x00, 0x7F}).intoInt());
-    }
-    
-    @Test
-    public void test_asBytes() {
-        assertArrayEquals(new int[] {0x00, 0x00, 0x00, 0x00},  new VLQNumber(0).intoByteArray());
-        assertArrayEquals(new int[] {0x00, 0x00, 0x00, 0x7F},  new VLQNumber(0x7F).intoByteArray());
+        assertEquals(0x00000000, new VLQNumber(new int[] {0x00, 0x00, 0x00, 0x00}).intoInt());
+        assertEquals(0x0000007F, new VLQNumber(new int[] {0x00, 0x00, 0x00, 0x7F}).intoInt());
+        assertEquals(0x00000080, new VLQNumber(new int[] {0x00, 0x00, 0x81, 0x00}).intoInt());
+        assertEquals(0x00002000, new VLQNumber(new int[] {0x00, 0x00, 0xC0, 0x00}).intoInt());
+        assertEquals(0x00003FFF, new VLQNumber(new int[] {0xFF, 0x7F}).intoInt());
+        assertEquals(0x00004000, new VLQNumber(new int[] {0x81, 0x80, 0x00}).intoInt());
+        assertEquals(0x001FFFFF, new VLQNumber(new int[] {0xFF, 0xFF, 0x7F}).intoInt());
+        assertEquals(0x00200000, new VLQNumber(new int[] {0x81, 0x80, 0x80, 0x00}).intoInt());
+        assertEquals(0x08000000, new VLQNumber(new int[] {0xC0, 0x80, 0x80, 0x00}).intoInt());
+        assertEquals(0x0FFFFFFF, new VLQNumber(new int[] {0xFF, 0xFF, 0xFF, 0x7F}).intoInt());
     }
 
+    @Test
+    public void test_asBytes() {
+        assertArrayEquals(new int[] {0x00}, new VLQNumber(0x00000000).intoByteArray());
+        assertArrayEquals(new int[] {0x7F}, new VLQNumber(0x0000007F).intoByteArray());
+        assertArrayEquals(new int[] {0x81, 0x00}, new VLQNumber(0x00000080).intoByteArray());
+        assertArrayEquals(new int[] {0xC0, 0x00}, new VLQNumber(0x00002000).intoByteArray());
+        assertArrayEquals(new int[] {0xFF, 0x7F}, new VLQNumber(0x00003FFF).intoByteArray());
+        assertArrayEquals(new int[] {0x81, 0x80, 0x00}, new VLQNumber(0x00004000).intoByteArray());
+        assertArrayEquals(new int[] {0xFF, 0xFF, 0x7F}, new VLQNumber(0x001FFFFF).intoByteArray());
+        assertArrayEquals(new int[] {0x81, 0x80, 0x80, 0x00}, new VLQNumber(0x00200000).intoByteArray());
+        assertArrayEquals(new int[] {0xC0, 0x80, 0x80, 0x00}, new VLQNumber(0x08000000).intoByteArray());
+        assertArrayEquals(new int[] {0xFF, 0xFF, 0xFF, 0x7F}, new VLQNumber(0x0FFFFFFF).intoByteArray());
+    }
 }
